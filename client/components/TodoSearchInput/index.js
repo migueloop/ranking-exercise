03631ba1f815c4d59f075ff265e568aeffe99e69
migueloop <myguelruiz@gmail.com>
2017-02-15 07:49:28 +0100

    Initial Commit

--- client/components/TodoSearchInput/index.js
+++ client/components/TodoSearchInput/index.js
@@ -26,16 +26,24 @@ class TodoSearchInput extends Component {
 
   handleShowInputSearch(e){
     e.preventDefault();
+    if(this.state.showInputSearch){
+       this.setState({ searchText: '' });
+       this.props.onSearch(this.state.searchText);
+    }
     this.setState({
       showInputSearch: !this.state.showInputSearch
     })
+  }
+
+  renderSearchOrCloseIcon(){
+   return !this.state.showInputSearch ? (<span className="fa fa-search"></span>) : (<span className="fa fa-times-circle"></span>)
   }
 
   render() {
     return (
       <div className={style.searchBar}>
         <button className={style.searchButton} type="button" onClick={::this.handleShowInputSearch}>
-          <span className="fa fa-search"></span>
+            {this.renderSearchOrCloseIcon()}
         </button>
         <div className={this.state.showInputSearch ? style.hide : style.show}>
           <strong className={style.todoTitle}> The todo-list exersise </strong>
